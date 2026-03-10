12382e2b02a56384a63dacffea9b12e4691cfd75
naman466 <namantyagi466@gmail.com>
2026-03-10 21:02:08 +0530

    feat(ir): add dtype_size and dtype_to_str helpers

--- src/core/tensor.h
+++ src/core/tensor.h
@@ -19,4 +19,33 @@ enum class DType : int32_t {
     FLOAT64   = 11,
 };
 
+
+inline std::string dtype_to_str(DType dt) {
+    switch (dt) {
+        case DType::FLOAT32: return "float";
+        case DType::FLOAT64: return "double";
+        case DType::INT32:   return "int32_t";
+        case DType::INT64:   return "int64_t";
+        case DType::INT8:    return "int8_t";
+        case DType::UINT8:   return "uint8_t";
+        case DType::INT16:   return "int16_t";
+        case DType::UINT16:  return "uint16_t";
+        case DType::BOOL:    return "uint8_t";
+        default:             return "void";
+    }
+}
+
+inline size_t dtype_size(DType dt) {
+    switch (dt) {
+        case DType::FLOAT32: return 4;
+        case DType::FLOAT64: return 8;
+        case DType::INT32:   return 4;
+        case DType::INT64:   return 8;
+        case DType::INT8:    return 1;
+        case DType::UINT8:   return 1;
+        case DType::INT16:   return 2;
+        case DType::UINT16:  return 2;
+        case DType::BOOL:    return 1;
+        default:             return 0;
+    }
 } // namespace null
